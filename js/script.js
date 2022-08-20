9b7cabc463b4f82b7bb39b1ab125dd04433cd238
Mateo SIndija <mtsindija05@gmail.com>
2022-08-20 17:29:57 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,59 @@
+const slide = (width, row) => {
+    row.css("transition", "transform 0.6s ease-in-out");
+    row.css("transform", `translateX(-${width}px, -50%)`);
+};
+
+const moveImgToEnd = (image, row) => {
+    image.hide(() => {
+        row.append(image.show());
+    });
+};
+
+const moveImgToBeggining = (image, row) => {
+    image.hide(() => {
+        row.prepend(image.show());
+    });
+};
+
+$(document).ready(() => {
+    const topRowImages = $(".container__images__top img");
+    const bottomRowImages = $(
+        ".container__images__bottom__imagesContainer img"
+    );
+
+    $(".container__images__bottom__buttonContainer__left").on("click", () => {
+        const firstTop = $(".container__images__top img").first();
+        const firstBottom = $(
+            ".container__images__bottom__imagesContainer img"
+        ).first();
+        const topRow = $(".container__images__top");
+        const bottomRow = $(".container__images__bottom__imagesContainer");
+
+        const firstTopWidth = firstTop.width();
+        const firstBottomWidth = firstBottom.width();
+
+        moveImgToEnd(firstTop, topRow);
+        moveImgToEnd(firstBottom, bottomRow);
+
+        slide(firstTopWidth, topRowImages);
+        slide(firstBottomWidth, bottomRowImages);
+    });
+
+    $(".container__images__bottom__buttonContainer__right").on("click", () => {
+        const lastTop = $(".container__images__top img").last();
+        const lastBottom = $(
+            ".container__images__bottom__imagesContainer img"
+        ).last();
+        const topRow = $(".container__images__top");
+        const bottomRow = $(".container__images__bottom__imagesContainer");
+
+        const lastTopWidth = lastTop.width();
+        const lastBottomWidth = lastBottom.width();
+
+        moveImgToBeggining(lastTop, topRow);
+        moveImgToBeggining(lastBottom, bottomRow);
+
+        slide(lastTopWidth, topRowImages);
+        slide(lastBottomWidth, bottomRowImages);
+    });
+});
