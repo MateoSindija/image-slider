3339930693771b15837a030d18123c6c6079fc36
Mateo SIndija <mtsindija05@gmail.com>
2022-08-25 19:03:10 +0200

    Fixed problems

--- js/script.js
+++ js/script.js
@@ -1,58 +1,42 @@
-const slide = (width, row) => {
-    row.css("transition", "transform 0.6s ease-in-out");
-    row.css("transform", `translateX(-${width}px, -50%)`);
-};
-
-const moveImgToEnd = (image, row) => {
-    image.hide(() => {
-        row.append(image.show());
-    });
-};
-
-const moveImgToBeggining = (image, row) => {
-    image.hide(() => {
-        row.prepend(image.show());
-    });
-};
+const SLIDE_DURATION = 200;
 
 $(document).ready(() => {
-    const topRowImages = $(".container__images__top img");
-    const bottomRowImages = $(
-        ".container__images__bottom__imagesContainer img"
-    );
+  const topRow = $(".container__images__top"); //top row container
+  const bottomRow = $(".container__images__bottom__imagesContainer"); //bottom row container
 
-    const topRow = $(".container__images__top");
-    const bottomRow = $(".container__images__bottom__imagesContainer");
-
-    $(".container__images__bottom__buttonContainer__left").on("click", () => {
-        const firstTop = $(".container__images__top img").first();
-        const firstBottom = $(
-            ".container__images__bottom__imagesContainer img"
-        ).first();
-
-        const firstTopWidth = firstTop.width();
-        const firstBottomWidth = firstBottom.width();
-
-        moveImgToEnd(firstTop, topRow);
-        moveImgToEnd(firstBottom, bottomRow);
-
-        slide(firstTopWidth, topRowImages);
-        slide(firstBottomWidth, bottomRowImages);
+  const moveImgToEnd = (image, row) => {
+    row.animate({ left: -image.width() }, SLIDE_DURATION, () => {
+      image.appendTo(row).show();
+      row.css("left", "");
     });
+  };
 
-    $(".container__images__bottom__buttonContainer__right").on("click", () => {
-        const lastTop = $(".container__images__top img").last();
-        const lastBottom = $(
-            ".container__images__bottom__imagesContainer img"
-        ).last();
-
-        const lastTopWidth = lastTop.width();
-        const lastBottomWidth = lastBottom.width();
-
-        moveImgToBeggining(lastTop, topRow);
-        moveImgToBeggining(lastBottom, bottomRow);
-
-        slide(lastTopWidth, topRowImages);
-        slide(lastBottomWidth, bottomRowImages);
+  const moveImgToBeggining = (image, row) => {
+    row.animate({ left: image.width() }, SLIDE_DURATION, () => {
+      image.prependTo(row);
+      row.css("left", "");
     });
+  };
+
+  $(".container__images__bottom__buttonContainer__left").on("click", () => {
+    const firstTop = $(".container__images__top img").first();
+    const firstBottom = $(
+      ".container__images__bottom__imagesContainer img"
+    ).first();
+
+    //move image to end
+    moveImgToEnd(firstTop, topRow);
+    moveImgToEnd(firstBottom, bottomRow);
+  });
+
+  $(".container__images__bottom__buttonContainer__right").on("click", () => {
+    const lastTop = $(".container__images__top img").last();
+    const lastBottom = $(
+      ".container__images__bottom__imagesContainer img"
+    ).last();
+
+    //move image to begging
+    moveImgToBeggining(lastTop, topRow);
+    moveImgToBeggining(lastBottom, bottomRow);
+  });
 });
